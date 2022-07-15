38d42d41393e94a77c430a913476c5c36f1f3bec
Shurgot <shurgot9@gmail.com>
2022-07-15 23:15:22 +0300

    Create password.js

---
+++ password.js
@@ -0,0 +1,17 @@
+function getRandom(max){
+    return Math.floor(Math.random() * max) // random number func
+}
+var K = 10 // length of password
+var passValue = "A,a,B,b,C,c,D,d,E,e,F,f,G,g,H,h,I,i,J,j,K,k,L,l,M,m,N,n,O,o,P,p,Q,q,R,r,S,s,T,t,U,u,V,v,W,w,X,x,Y,y,Z,z,1,2,3,4,5,6,7,8,9"// database of symbols for password
+var passValueSp = passValue.split(",")
+var passResult = []
+var passFinal = ""
+for(let i = 0; i < K; i++){
+    passResult.push(getRandom(passValueSp.length)) // choose random index from value, and put them to empty array passResult
+    var pass = passValueSp[passResult[i]] // take index and convert it to symbols from value
+    passFinal += pass
+}
+
+passFinal += passValueSp[getRandom(passValueSp.length)] 
+
+console.log(passFinal)
